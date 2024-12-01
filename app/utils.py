12e88df6495bd75533319c5cc29b58a0de561045
mehtachandrashekhar <mehtachandrashekhar84@gmail.com>
2024-12-01 21:51:19 +0530

    fix: utils rate limiting

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,27 @@
-# app/utils.py
 import time
+import functools
 
-def rate_limit(delay=1):
-    """Simple rate limiter with a delay in seconds."""
-    time.sleep(delay)
+def rate_limit(max_calls=1, period_seconds=1):
+    """Simple rate limiter with a max number of calls and a period in seconds."""
+    def decorator(func):
+        calls = [0]
+        last_call = [time.time()]
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            current_time = time.time()
+            elapsed_time = current_time - last_call[0]
+            calls[0] += 1
+            
+            if elapsed_time >= period_seconds:
+                last_call[0] = current_time
+                calls[0] = 1
+            else:
+                if calls[0] > max_calls:
+                    time.sleep(period_seconds - elapsed_time)
+                else:
+                    calls[0] += 1
+
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
